e5b06a63b6967b2fa72bc6cdf7a528565b569055
krispypatata <ksgabinete@up.edu.ph>
2024-08-28 17:39:23 +0800

    chore: ADD sample code for array

---
+++ 01 - Introduction to COBOL/array.cob
@@ -0,0 +1,28 @@
+      *DESCRIPTION: 
+      *AUTHOR: Keith Ginoel S. Gabinete
+      *DATE: August 28, 2024 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. array.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+           WORKING-STORAGE SECTION.
+           01 ARR-REC.
+               02 MY-ARR PIC A(10) OCCURS 10 TIMES.
+           
+           77 IDX PIC 99 VALUE 1.
+
+       PROCEDURE DIVISION.
+      *COBOL indexing starts with 1
+           MOVE "Hello" TO MY-ARR(1).
+           MOVE "World" TO MY-ARR(10).
+
+           PERFORM 10 times
+               DISPLAY IDX " " MY-ARR(IDX)
+               COMPUTE IDX = IDX + 1
+      *this is also valid for incrementing IDX: ADD 1 to IDX
+           END-PERFORM.
+
+           STOP RUN.
+        
