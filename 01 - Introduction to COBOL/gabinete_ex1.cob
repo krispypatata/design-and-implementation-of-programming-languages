bcb5c7d87486574e1ba5e7bc27ada258354cc2e3
krispypatata <ksgabinete@up.edu.ph>
2024-08-29 23:12:08 +0800

    fix: FIX input validation; exer1 is now fully working

--- 01 - Introduction to COBOL/gabinete_ex1.cob
+++ 01 - Introduction to COBOL/gabinete_ex1.cob
@@ -1,8 +1,7 @@
-      *DESCRIPTION: This program computes the factorial of the 
-      *             largest number from an array of 5 single-digit
-      *             positive integers.
+      *DESCRIPTION: This program computes the factorial of the largest 
+      *number from an array of 5 single-digit positive integers.
       *AUTHOR: Keith Ginoel S. Gabinete
-      *DATE: August 28, 2024 
+      *DATE: August 29, 2024 
        IDENTIFICATION DIVISION.
        PROGRAM-ID. gabinete_ex1.
 
@@ -19,12 +18,14 @@
       *easier for us to check later if the array is still empty or not 
       *(for printing its contents)
        01 numRecord.
-        02 numArr PIC 9(1) OCCURS 5 TIMES VALUE 0.
-           
-       77 choice PIC 9(1) VALUE 1.
+           02 numArr PIC 9(1) OCCURS 5 TIMES VALUE 0.
+    
+       77 userChoice PIC X(38).
+       77 choice PIC 9(38) VALUE 1.
        77 exited PIC 9(1) VALUE 0.
 
-       77 userInput PIC 9(2).
+       77 userInput PIC X(38).
+       77 numericInput PIC 9(38).
        77 isValid PIC 9(1) VALUE 0.
        77 validInput PIC 9(1).
 
@@ -40,7 +41,7 @@
       *NOTE: COBOL indexing starts with 1
        PROCEDURE DIVISION.
            PERFORM displayMenu UNTIL exited = 1.
-      *Terminate the program
+      *    Terminate the program
            STOP RUN.
       
       *A function to display the main menu
@@ -52,60 +53,87 @@
            DISPLAY "[4] Exit".
 
            DISPLAY "Choice: " WITH NO ADVANCING.
-           ACCEPT choice.
-
-           IF choice = 1
-               PERFORM fillNumArray
-           ELSE
-               IF choice = 2
-                   PERFORM printNumArray
+           ACCEPT userChoice.
+           
+      *    Use the function trim to remove any leading or tailing whitespaces
+      *    <whiespace/s>1<whiespace/s> is accepted
+           IF FUNCTION TRIM(userChoice) IS NUMERIC
+      *        Use the function numval to get the actual numeric value of the 
+      *        given string (since it already passed the IS NUMERIC check)     
+               MOVE FUNCTION NUMVAL(userChoice) TO choice
+               IF choice = 1
+                   PERFORM fillNumArray
                ELSE
-                   IF choice = 3
-                       PERFORM getFactorialOfLargestNum
+                   IF choice = 2
+                       PERFORM printNumArray
                    ELSE
-                       IF choice = 4
-                           DISPLAY " "
-                           DISPLAY "EXITED!"
-                           MOVE 1 TO exited
+                       IF choice = 3
+                           PERFORM getFactorialOfLargestNum
                        ELSE
-                           DISPLAY " "
-                           DISPLAY "Invalid Input!"
+                           IF choice = 4
+                               DISPLAY " "
+                               DISPLAY "EXITED!"
+                               MOVE 1 TO exited
+                           ELSE
+                               DISPLAY " "
+                               DISPLAY "Invalid Input!"
+                           END-IF
                        END-IF
                    END-IF
                END-IF
+           ELSE
+               DISPLAY " "
+               DISPLAY "Invalid Input!"
            END-IF.
       
-      *New line
+      *    New line
            DISPLAY " ".
-
 
       *A function to fill the num array with positive integer inputs
       *ref: https://stackoverflow.com/questions/28167441/how-to-check-
       *valid-numeric-in-number-for-a-given-length
+      *ref: https://stackoverflow.com/questions/60457084/how-to-validate-a-
+      *negative-value-being-passed-in-as-a-alphanumeric-literal
        fillNumArray.
            DISPLAY " ".
            DISPLAY "FILL ARRAY".
 
            MOVE 1 TO iterator.
            PERFORM UNTIL iterator > 5
-      * Check if the input entered by the user is a valid single digit input
+      *        Check if the input entered by the user is a valid 
+      *        single digit input
+      *        Loop until it encounters a valid input         
                MOVE 0 to isValid
                PERFORM UNTIL isValid = 1
+                   DISPLAY "(" iterator ") " WITH NO ADVANCING
                    DISPLAY "Enter a positive integer (1-9): " 
                    WITH NO ADVANCING
                    ACCEPT userInput
                    
-                   IF userInput IS NUMERIC
-                       IF userInput >= 1 AND userInput <= 9
-                           MOVE userInput TO validInput
+      *            Use the function trim to remove any leading or 
+      *            tailing whitespaces
+      *            <whiespace/s>8<whiespace/s> is accepted
+                   IF FUNCTION TRIM (userInput) IS NUMERIC
+      *                Use the function numval to get the actual 
+      *                numeric value of the given string (since it 
+      *                already passed the IS NUMERIC check)
+                       MOVE FUNCTION NUMVAL(userInput) TO numericInput
+      *                Input must be a single positive integer only
+                       IF (numericInput >= 1 AND numericInput <= 9)
+                           MOVE numericInput TO validInput
                            MOVE validInput TO numArr(iterator)
                            MOVE 1 TO isValid
                        ELSE
+                           DISPLAY " "
                            DISPLAY "INVALID INPUT"
                            DISPLAY "Must be within the given range."
+                           DISPLAY " "
+                       END-IF
                    ELSE
+                       DISPLAY " "
                        DISPLAY "INVALID INPUT!"
                        DISPLAY "Must be a positive integer input only."
+                       DISPLAY " "
                    END-IF
                END-PERFORM
 
@@ -115,7 +143,7 @@
       *A function to print the numbers in the array
        printNumArray.
            DISPLAY " ".
-      *Check first if the array is still empty
+      *    Check first if the array is still empty
            IF numArr(1) = 0
                DISPLAY "ARRAY IS STILL EMPTY!"
                DISPLAY "Fill the array with positive integers first!"
@@ -131,7 +159,8 @@
                DISPLAY " "
            END-IF.
 
-      *A function to update the value of the largest number
+      *A function to get the value of the largest number in the
+      *initialized num array
        getLargestNum.
            MOVE 1 TO iterator.
            PERFORM UNTIL iterator > 5
@@ -144,15 +173,26 @@
 
       *A function to solve for the factorial of the largest number in the array
        getFactorialOfLargestNum.
-           PERFORM getLargestNum.
+           DISPLAY " ".
 
-           MOVE 1 TO nFactorial.
-           PERFORM UNTIL largestNum = 0
-               COMPUTE nFactorial = nFactorial * largestNum
-               COMPUTE largestNum = largestNum - 1
-           END-PERFORM.
+           IF numArr(1) = 0
+               DISPLAY "ARRAY IS STILL EMPTY!"
+               DISPLAY "Fill the array with positive integers first!"
+           ELSE
+      *        Update the value of the largest number variable
+               PERFORM getLargestNum
+               
+               DISPLAY "FACTORIAL OF LARGEST NUMBER"
+               DISPLAY largestNum "! = " WITH NO ADVANCING
+
+      *        Compute for the factorial of the largest number
+               MOVE 1 TO nFactorial
+               PERFORM UNTIL largestNum = 0
+                   COMPUTE nFactorial = nFactorial * largestNum
+                   COMPUTE largestNum = largestNum - 1
+               END-PERFORM
            
-           MOVE nFactorial TO nFactorialFormatted
-           DISPLAY " "
-           DISPLAY "FACTORIAL OF LARGEST NUMBER"
-           DISPLAY FUNCTION TRIM(nFactorialFormatted LEADING).
+               MOVE nFactorial TO nFactorialFormatted
+               DISPLAY FUNCTION TRIM(nFactorialFormatted LEADING)
+           END-IF.
+
